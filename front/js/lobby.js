ab832c16487588061309fa0a363757360219f52a
P0TEZ <60687263+P0TEZ@users.noreply.github.com>
2021-04-28 19:07:38 +0200

    style lobby

--- front/js/lobby.js
+++ front/js/lobby.js
@@ -1,20 +1,23 @@
 const gameId = document.getElementById('gameId'); 
+const speedRange = document.getElementById('speedRange');
+const speedOutput = document.getElementById('speed');
+const colorSelector = document.getElementById('color');
+speedOutput.value = speedRange.value;
+const colors = [
+    '#ffe119',
+    '#f58231',
+    '#000075',
+    '#e6194B',
+    '#a9a9a9',
+    '#aaffc3',
+    '#800000',
+    '#42d4f4',
+    '#469990',
+    '#4363d8',
+    '#911eb4',
+];
 
-
-/**************AUDIO*******************************/
-let moveaudio = new Audio('../audio/salon.mp3');
-moveaudio.volume=.1;
-function salonAudio(){
-    moveaudio.play();
-}
-/**************************************************/
-socket.emit('getRoomInfo');
-socket.on('roomInfo',(data) => {
-    console.log(data);
-    salonAudio();
-    showId(data.id);
-})
-
+/*******************GAME CODE**********************/
 gameId.addEventListener('click',()=>{
     let copyText = document.getElementById("code");
     copyText.select();
@@ -35,5 +38,33 @@ function showId(id){
     gameId.firstElementChild.value = id;
 }
 
+/**************************SETTINGS*****************/
+speedRange.oninput = function(){
+    speedOutput.value = this.value;
+}
+speedOutput.oninput = function(){
+    speedRange.value = this.value;
+}
+
+colorSelector.oninput = function(){
+    let color = colors[this.value];
+    let s = document.createElement("style");
+    document.head.appendChild(s);
+    s.textContent = '.color::-webkit-slider-thumb{background-color:'+color+' }'
+}
 
 
+
+/**************AUDIO*******************************/
+let moveaudio = new Audio('../audio/salon.mp3');
+moveaudio.volume=.1;
+function salonAudio(){
+    moveaudio.play();
+}
+/**************************************************/
+socket.emit('getRoomInfo');
+socket.on('roomInfo',(data) => {
+    console.log(data);
+    salonAudio();
+    showId(data.id);
+})
