353d2c42b6696c36cce883920d955b74f30b6162
Bidemboum <pierre.villain@student.yncrea.fr>
2021-05-24 16:44:05 +0200

    Nouveaux déchets et affiche

--- front/js/game/dechets_DATA.js
+++ front/js/game/dechets_DATA.js
@@ -2,15 +2,15 @@ const dechets = [
     { id: 0, name: 'Burger', url: '../../assets/dechets/burger.png', id_poubelle: 'Marron'},  
     { id: 1, name: 'Trognon de Pomme', url: '../../assets/dechets/pomme.png', id_poubelle: 'Marron'},
     { id: 2, name: 'Feuille', url: '../../assets/dechets/feuille.png', id_poubelle: 'Marron'},
-    { id: 3, name: 'Papier Toilette', url: '../../assets/dechets/pq.png', id_poubelle: 'Jaune'},
-    { id: 4, name: 'Journal', url: '../../assets/dechets/journal.png', id_poubelle: 'Jaune'},
-    { id: 5, name: 'Boite en Carton', url: '../../assets/dechets/carton.png', id_poubelle: 'Jaune'},
-    { id: 6, name: 'Sac Plastique', url: '../../assets/dechets/sac.png', id_poubelle: 'Vert'},
-    { id: 7, name: 'Bouteille plastique', url: '../../assets/dechets/water_bottle.png', id_poubelle: 'Vert'},
-    { id: 8, name: 'Gobelet Plastique', url: '../../assets/dechets/cup.png', id_poubelle: 'Vert'},
-    { id: 9, name: 'Bouteille en verre', url: '../../assets/dechets/bouteille_verre.png', id_poubelle: 'Bleue'},
-    { id: 10, name: 'Bocal en Verre', url: '../../assets/dechets/verre.png', id_poubelle: 'Bleue'},
-    { id: 11, name: 'Bouteille de Biere', url: '../../assets/dechets/bouteille_biere.png', id_poubelle: 'Bleue'},
+    { id: 3, name: 'Papier Toilette', url: '../../assets/dechets/pq.png', id_poubelle: 'Bleue'},
+    { id: 4, name: 'Journal', url: '../../assets/dechets/journal.png', id_poubelle: 'Bleue'},
+    { id: 5, name: 'Boite en Carton', url: '../../assets/dechets/carton.png', id_poubelle: 'Bleue'},
+    { id: 6, name: 'Sac Plastique', url: '../../assets/dechets/sac.png', id_poubelle: 'Jaune'},
+    { id: 7, name: 'Bouteille plastique', url: '../../assets/dechets/water_bottle.png', id_poubelle: 'Jaune'},
+    { id: 8, name: 'Gobelet Plastique', url: '../../assets/dechets/cup.png', id_poubelle: 'Jaune'},
+    { id: 9, name: 'Bouteille en verre', url: '../../assets/dechets/bouteille_verre.png', id_poubelle: 'Vert'},
+    { id: 10, name: 'Bocal en Verre', url: '../../assets/dechets/verre.png', id_poubelle: 'Vert'},
+    { id: 11, name: 'Bouteille de Biere', url: '../../assets/dechets/bouteille_biere.png', id_poubelle: 'Vert'},
     { id: 12, name: 'Canette Soda', url: '../../assets/dechets/canette.png', id_poubelle: 'Violet'},
     { id: 13, name: 'Conserve', url: '../../assets/dechets/conserve.png', id_poubelle: 'Violet'},
     { id: 14, name: 'Petite Conserve', url: '../../assets/dechets/conserve_thon.png', id_poubelle: 'Violet'},
@@ -20,9 +20,9 @@ const dechets = [
     { id: 18, name: 'Pillule', url: '../../assets/dechets/pillule.png', id_poubelle: 'Blanc'},
     { id: 19, name: 'Boite de pillules', url: '../assets/dechets/boite_pillules.png', id_poubelle: 'Blanc'},
     { id: 20, name: 'Médicament', url: '../../assets/dechets/plaquette_pillules.png', id_poubelle: 'Blanc'},
-    { id: 21, name: 'Vieux Tee-shirt', url: '../../assets/dechets/tee_shirt.png', id_poubelle: 'Noir'},
-    { id: 22, name: 'Bottes usagees', url: '../../assets/dechets/botte.png', id_poubelle: 'Noir'},
-    { id: 23, name: 'Vieux Talons', url: '../../assets/dechets/talon.png', id_poubelle: 'Noir'},
+    { id: 21, name: 'Boite à pizza', url: '../../assets/dechets/pizza.png', id_poubelle: 'Noir'},
+    { id: 22, name: 'Yaourt', url: '../../assets/dechets/yaourt.png', id_poubelle: 'Noir'},
+    { id: 23, name: 'Mouchoirs', url: '../../assets/dechets/mouchoirs.png', id_poubelle: 'Noir'},
     { id: 24, name: 'Une pile', url: '../../assets/dechets/pile.png', id_poubelle: 'Rouge'},
     { id: 25, name: 'Bombe de Peinture', url: '../../assets/dechets/peinture.png', id_poubelle: 'Rouge'},
     { id: 26, name: 'Bidon d\'essence', url: '../../assets/dechets/bidon_essence.png', id_poubelle: 'Rouge'},
@@ -31,13 +31,13 @@ const dechets = [
 export {dechets}
 
 /*id_poubelle : 
-Bleue = Verre
+Bleue = Papier/Carton
 Violet = Metal
 Marron = Organique
-Jaune = Papier/Carton 
+Jaune = Plastique
 Noir = Non recyclabes
 Rouge = Poubelles Spéciales 
 Orange = Dechets Electroniques
 Blanc = medicaments
-Vert = Plastiques
+Vert = Verre
 */
